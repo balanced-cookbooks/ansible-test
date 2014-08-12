213024ac4a8320eae12826365aeb18c529c75fda
Marshall Jones <marshall@balancedpayments.com>
2014-08-11 20:43:31 -0700

    working fixes https://github.com/neillturner/kitchen-ansible/issues/1

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -4,14 +4,10 @@ require 'serverspec'
 include Serverspec::Helper::Exec
 include Serverspec::Helper::DetectOS
 
-describe command('nginx --version') do
+describe command('nginx -v') do
   it { should return_exit_status 0 }
 end
 
 describe service('nginx') do
-  it { should be_running }
-end
-
-describe service('pooopopsdf') do
-  it { should be_running }
+  it { should_not be_running }
 end
